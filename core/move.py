70857491692050ec447140e87ddb38429a710b08
jeremytrane <114171300+jeremytrane@users.noreply.github.com>
2025-04-02 18:18:39 +0100

    Added piece promotion template

--- core/move.py
+++ core/move.py
@@ -1,9 +1,10 @@
 class Move:
-    def __init__(self, from_pos: tuple[int, int], to_pos: tuple[int, int], piece, captured=None):
+    def __init__(self, from_pos, to_pos, piece, captured=None, promotion=None):
         self.from_pos = from_pos
         self.to_pos = to_pos
         self.piece = piece
         self.captured = captured
+        self.promotion = promotion
 
     def __repr__(self):
         fr = f"{chr(self.from_pos[1] + ord('a'))}{8 - self.from_pos[0]}"
