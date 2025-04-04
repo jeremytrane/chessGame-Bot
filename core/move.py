5507caefbabdf4af7267afe072bd7bbcdb810e16
jeremytrane <114171300+jeremytrane@users.noreply.github.com>
2025-04-04 16:56:02 +0100

    Updated for en passant

--- core/move.py
+++ core/move.py
@@ -1,10 +1,11 @@
 class Move:
-    def __init__(self, from_pos, to_pos, piece, captured=None, promotion=None):
+    def __init__(self, from_pos, to_pos, piece, captured=None, promotion=None, captured_pos=None):
         self.from_pos = from_pos
         self.to_pos = to_pos
         self.piece = piece
         self.captured = captured
         self.promotion = promotion
+        self.captured_pos = captured_pos if captured_pos else to_pos  # Default to destination
 
     def __repr__(self):
         fr = f"{chr(self.from_pos[1] + ord('a'))}{8 - self.from_pos[0]}"
