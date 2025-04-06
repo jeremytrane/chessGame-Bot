08a5cdad0c46351979df3286342883fd87571f73
jeremytrane <114171300+jeremytrane@users.noreply.github.com>
2025-04-06 09:54:48 +0100

    Updated undoing caslte move

--- core/move.py
+++ core/move.py
@@ -1,11 +1,12 @@
 class Move:
-    def __init__(self, from_pos, to_pos, piece, captured=None, promotion=None, captured_pos=None):
+    def __init__(self, from_pos, to_pos, piece, captured=None, promotion=None, captured_pos=None, castling=False):
         self.from_pos = from_pos
         self.to_pos = to_pos
         self.piece = piece
         self.captured = captured
         self.promotion = promotion
-        self.captured_pos = captured_pos if captured_pos else to_pos  # Default to destination
+        self.captured_pos = captured_pos if captured_pos else to_pos
+        self.castling = castling  # 🔥 new flag
 
     def __repr__(self):
         fr = f"{chr(self.from_pos[1] + ord('a'))}{8 - self.from_pos[0]}"
